51a1519b04ab4734e4d9deaaafede419fc11a390
AMF7816 <anibal.flores7816@gmail.com>
2015-11-24 18:26:36 -0200

    Tutorial
    Falla cuando se baja el scroll y se elige otro boton porque queda en
    pantalla el scroll, no se como arreglarlo

--- Interfaces/Tutorial.Designer.cs
+++ Interfaces/Tutorial.Designer.cs
@@ -39,18 +39,20 @@
             this.btnUtilidad = new System.Windows.Forms.Button();
             this.btnFuncion = new System.Windows.Forms.Button();
             this.btnCriterios = new System.Windows.Forms.Button();
+            this.rtbTitulo = new System.Windows.Forms.RichTextBox();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // rtbMensaje
             // 
             this.rtbMensaje.BackColor = System.Drawing.SystemColors.ActiveCaption;
-            this.rtbMensaje.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.rtbMensaje.Location = new System.Drawing.Point(12, 319);
+            this.rtbMensaje.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbMensaje.Font = new System.Drawing.Font("Century Gothic", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbMensaje.Location = new System.Drawing.Point(60, 368);
             this.rtbMensaje.Name = "rtbMensaje";
             this.rtbMensaje.ReadOnly = true;
-            this.rtbMensaje.Size = new System.Drawing.Size(956, 177);
-            this.rtbMensaje.TabIndex = 0;
+            this.rtbMensaje.Size = new System.Drawing.Size(830, 128);
+            this.rtbMensaje.TabIndex = 99;
             this.rtbMensaje.Text = "";
             // 
             // btnSalir
@@ -58,11 +60,11 @@
             this.btnSalir.BackColor = System.Drawing.SystemColors.Highlight;
             this.btnSalir.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.btnSalir.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnSalir.Location = new System.Drawing.Point(856, 522);
+            this.btnSalir.Location = new System.Drawing.Point(778, 522);
             this.btnSalir.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.btnSalir.Name = "btnSalir";
             this.btnSalir.Size = new System.Drawing.Size(112, 35);
-            this.btnSalir.TabIndex = 12;
+            this.btnSalir.TabIndex = 8;
             this.btnSalir.Text = "Salir";
             this.btnSalir.UseVisualStyleBackColor = false;
             this.btnSalir.Click += new System.EventHandler(this.btnSalir_Click);
@@ -75,7 +77,7 @@
             this.btnConsultar.Location = new System.Drawing.Point(366, 235);
             this.btnConsultar.Name = "btnConsultar";
             this.btnConsultar.Size = new System.Drawing.Size(200, 56);
-            this.btnConsultar.TabIndex = 14;
+            this.btnConsultar.TabIndex = 6;
             this.btnConsultar.Text = "Consultar Propiedad";
             this.btnConsultar.UseVisualStyleBackColor = false;
             this.btnConsultar.Click += new System.EventHandler(this.btnConsultar_Click);
@@ -88,7 +90,7 @@
             this.btnModificar.Location = new System.Drawing.Point(690, 235);
             this.btnModificar.Name = "btnModificar";
             this.btnModificar.Size = new System.Drawing.Size(200, 56);
-            this.btnModificar.TabIndex = 15;
+            this.btnModificar.TabIndex = 7;
             this.btnModificar.Text = "Modificar Propiedad";
             this.btnModificar.UseVisualStyleBackColor = false;
             this.btnModificar.Click += new System.EventHandler(this.btnModificar_Click);
@@ -101,7 +103,7 @@
             this.btnSeleccion.Location = new System.Drawing.Point(690, 134);
             this.btnSeleccion.Name = "btnSeleccion";
             this.btnSeleccion.Size = new System.Drawing.Size(200, 56);
-            this.btnSeleccion.TabIndex = 16;
+            this.btnSeleccion.TabIndex = 4;
             this.btnSeleccion.Text = "Selección de Alternativas";
             this.btnSeleccion.UseVisualStyleBackColor = false;
             this.btnSeleccion.Click += new System.EventHandler(this.btnSeleccion_Click);
@@ -114,7 +116,7 @@
             this.btnABM.Location = new System.Drawing.Point(60, 235);
             this.btnABM.Name = "btnABM";
             this.btnABM.Size = new System.Drawing.Size(200, 56);
-            this.btnABM.TabIndex = 17;
+            this.btnABM.TabIndex = 5;
             this.btnABM.Text = "Registro de Propiedad";
             this.btnABM.UseVisualStyleBackColor = false;
             this.btnABM.Click += new System.EventHandler(this.btnABM_Click);
@@ -137,7 +139,7 @@
             this.btnUtilidad.Location = new System.Drawing.Point(60, 27);
             this.btnUtilidad.Name = "btnUtilidad";
             this.btnUtilidad.Size = new System.Drawing.Size(200, 56);
-            this.btnUtilidad.TabIndex = 18;
+            this.btnUtilidad.TabIndex = 1;
             this.btnUtilidad.Text = "¿Para qué sirve?";
             this.btnUtilidad.UseVisualStyleBackColor = false;
             this.btnUtilidad.Click += new System.EventHandler(this.btnUtilidad_Click);
@@ -150,7 +152,7 @@
             this.btnFuncion.Location = new System.Drawing.Point(690, 27);
             this.btnFuncion.Name = "btnFuncion";
             this.btnFuncion.Size = new System.Drawing.Size(200, 56);
-            this.btnFuncion.TabIndex = 19;
+            this.btnFuncion.TabIndex = 2;
             this.btnFuncion.Text = "¿Cómo funciona?";
             this.btnFuncion.UseVisualStyleBackColor = false;
             this.btnFuncion.Click += new System.EventHandler(this.btnFuncion_Click);
@@ -163,10 +165,23 @@
             this.btnCriterios.Location = new System.Drawing.Point(60, 134);
             this.btnCriterios.Name = "btnCriterios";
             this.btnCriterios.Size = new System.Drawing.Size(200, 56);
-            this.btnCriterios.TabIndex = 20;
+            this.btnCriterios.TabIndex = 3;
             this.btnCriterios.Text = "Criterios Analizados";
             this.btnCriterios.UseVisualStyleBackColor = false;
             this.btnCriterios.Click += new System.EventHandler(this.btnCriterios_Click);
+            // 
+            // rtbTitulo
+            // 
+            this.rtbTitulo.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.rtbTitulo.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.rtbTitulo.Font = new System.Drawing.Font("Century Gothic", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rtbTitulo.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.rtbTitulo.Location = new System.Drawing.Point(60, 321);
+            this.rtbTitulo.Name = "rtbTitulo";
+            this.rtbTitulo.ReadOnly = true;
+            this.rtbTitulo.Size = new System.Drawing.Size(830, 41);
+            this.rtbTitulo.TabIndex = 98;
+            this.rtbTitulo.Text = "";
             // 
             // Tutorial
             // 
@@ -174,6 +189,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(982, 571);
+            this.Controls.Add(this.rtbTitulo);
             this.Controls.Add(this.btnCriterios);
             this.Controls.Add(this.btnFuncion);
             this.Controls.Add(this.btnUtilidad);
@@ -204,6 +220,7 @@
         private System.Windows.Forms.Button btnUtilidad;
         private System.Windows.Forms.Button btnFuncion;
         private System.Windows.Forms.Button btnCriterios;
+        private System.Windows.Forms.RichTextBox rtbTitulo;
 
     }
 }
--- Interfaces/Tutorial.cs
+++ Interfaces/Tutorial.cs
@@ -23,37 +23,44 @@ namespace AlquileresDEC.Interfaces
 
         private void btnUtilidad_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "¿Para qué sirve?\r\rLa aplicación utiliza la técnica TOPSIS para determinar el orden de preferencia en el que se recomienda alquilar una propiedad.";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b¿Para qué sirve?\b0}";
+            rtbMensaje.Text = "La aplicación utiliza la técnica TOPSIS para determinar el orden de preferencia en el que se recomienda alquilar una propiedad.";
         }
 
         private void btnFuncion_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "¿Cómo funciona?\r\rEl programa emplea una base de datos que cuenta con alrededor de 30 departamentos extraídos de diversas inmobiliarias. Es posible filtrar los departamentos en función de los siguientes criterios: precio, barrio, estado, requisitos, servicios, antigüedad y número de habitaciones.\rEn cada uno de estos criterios se puede determinar su objetivo indicando si se desea maximizar o minimizar. Además el usuario puede establecer el peso de cada uno.";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b¿Cómo funciona?\b0}";
+            rtbMensaje.Text = "El programa emplea una base de datos que cuenta con alrededor de 30 departamentos extraídos de diversas inmobiliarias. Es posible filtrar los departamentos en función de los siguientes criterios: precio, barrio, estado, requisitos, servicios, antigüedad y número de habitaciones.\rEn cada uno de estos criterios se puede determinar su objetivo indicando si se desea maximizar o minimizar. Además el usuario puede establecer el peso de cada uno.";
         }
 
         private void btnCriterios_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "Criterios Analizados: \r\r- Precio: Indica el costo monetario para alquilar una propiedad (Casa o Departamento).\r\r- Barrio: Representa la ubicación de la propiedad (Alberdi, Alta Córdoba, Barrio Jardín, Centro, Cerro de las Rosas, Cofico, General Paz, Nueva Córdoba).\r\r- Estado: Indica las condiciones en las que se encuentra la propiedad (Malo, Regular, Bueno, Muy Bueno, Excelente).\r\r- Requisitos: Indica las condiciones necesarias para alquilar una propiedad (garantía propietaria de familiar, recibo de sueldo, documentación, etc.). En función de la cantidad de requisitos solicitados se los clasifica como: Ninguno, Pocos, Algunos, Bastantes, Muchos.\r\r- Servicios: Representa las comodidades que ofrece la propiedad (estacionamiento, muebles, asador, pileta, tv con cable, internet, etc.). En función de la cantidad de servicios solicitados se los clasifica como: Ninguno, Pocos, Algunos, Bastantes, Muchos.\r\r- Antigüedad: Representa el tiempo transcurrido desde su inauguración.\r\r- Número de habitaciones: Indica de cuantas habitaciones dispone la propiedad (1,2, ..., 6).";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b Criterios Analizados\b0}";
+            rtbMensaje.Text = "- Precio: Indica el costo monetario para alquilar una propiedad (Casa o Departamento).\r\r- Barrio: Representa la ubicación de la propiedad (Alberdi, Alta Córdoba, Barrio Jardín, Centro, Cerro de las Rosas, Cofico, General Paz, Nueva Córdoba).\r\r- Estado: Indica las condiciones en las que se encuentra la propiedad (Malo, Regular, Bueno, Muy Bueno, Excelente).\r\r- Requisitos: Indica las condiciones necesarias para alquilar una propiedad (garantía propietaria de familiar, recibo de sueldo, documentación, etc.). En función de la cantidad de requisitos solicitados se los clasifica como: Ninguno, Pocos, Algunos, Bastantes, Muchos.\r\r- Servicios: Representa las comodidades que ofrece la propiedad (estacionamiento, muebles, asador, pileta, tv con cable, internet, etc.). En función de la cantidad de servicios solicitados se los clasifica como: Ninguno, Pocos, Algunos, Bastantes, Muchos.\r\r- Antigüedad: Representa el tiempo transcurrido desde su inauguración.\r\r- Número de habitaciones: Indica de cuantas habitaciones dispone la propiedad (1,2, ..., 6).";
         }
         
         private void btnABM_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "Registro de Propiedad:\r\rPermite dar de alta una propiedad completando los campos obligatorios indicados con *.";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b Registro de Propiedad\b0}";
+            rtbMensaje.Text = "Permite dar de alta una propiedad completando los campos obligatorios indicados con *.";
         }
 
         private void btnSeleccion_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "Selección de Alternativas:\r\rPermite realizar la búsqueda filtrando por tipo de propiedad, localidad, barrio, precio y número de habitaciones para mostrar las alternativas candidatas. De esas alternativas candidatas se pueden agregar una en particular o todas las alternativas elegidas. Además se deben seleccionar los criterios indicando su objetivo y el peso de cada criterio.";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b Selección de Alternativas\b0}";
+            rtbMensaje.Text = "Permite realizar la búsqueda filtrando por tipo de propiedad, localidad, barrio, precio y número de habitaciones para mostrar las alternativas candidatas. De esas alternativas candidatas se pueden agregar una en particular o todas las alternativas elegidas. Además se deben seleccionar los criterios indicando su objetivo y el peso de cada criterio.";
         }
 
         private void btnConsultar_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "Consultar Propiedad:\r\rPermite realizar la búsqueda de propiedades filtrando por tipo de propiedad, localidad, barrio, precio y número de habitaciones.";
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b Consultar Propiedad \b0}";
+            rtbMensaje.Text = "Permite realizar la búsqueda de propiedades filtrando por tipo de propiedad, localidad, barrio, precio y número de habitaciones.";
         }
 
         private void btnModificar_Click(object sender, EventArgs e)
         {
-            rtbMensaje.Text = "Modificar Propiedad:\r\rPermite modificar los datos de registro de una propiedad.";       
+            rtbTitulo.Rtf = @"{\rtf1\ansi \b Modificar Propiedad \b0}";
+            rtbMensaje.Text = "Permite modificar los datos de registro de una propiedad.";       
         }
     }
 }
